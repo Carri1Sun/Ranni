fe1e2105cbbac1fd254c236b6054cfc1dae6ee07
Kaiyi <skic6039@gmail.com>
2026-04-26 21:38:38 +0800

    chore: - Add project ignore rules - Stop tracking generated files - Stop tracking OS metadata - Tests: not run (ignore-only change)

--- .next/dev/static/chunks/app_page_tsx_0copofb._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/components_13mbkni._.js","static/chunks/node_modules_micromark-core-commonmark_dev_lib_13dj~6i._.js","static/chunks/node_modules_01i9f80._.js","static/chunks/components_0lridfp._.css"],
-    source: "dynamic"
-});
